61be4420c109fdf0dc6165513165c276e930189f
Ondřej Procházka <ony.prochy@gmail.com>
2021-09-19 13:00:48 +0200

    Added task types

---
+++ Tasks/Tasks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tasks
+{
+
+    public abstract class Task
+    {
+        public string Name { get; set; }
+        public bool IsFinished { get; set; }
+        public TimeSpan RemindTime { get; set; }
+
+    }
+
+    public class SimpleTask : Task
+    {   
+        DateTime dueTime { get; set; }
+    }
+
+    public class DailyTask : SimpleTask
+    {
+
+    }
+
+    public class WeeklyTask : SimpleTask
+    {
+        DayOfWeek[] days { get; set; }
+
+    }
+
+    public class MonthlyTask : SimpleTask
+    {
+        int[] days { get; set; }
+    }
+
+    public class YearlyTask : SimpleTask
+    {
+        int month { get; set; }
+        int day { get; set; }
+    }
+
+
+    public class CustomTask : Task
+    {
+        //TODO
+    }
+}
