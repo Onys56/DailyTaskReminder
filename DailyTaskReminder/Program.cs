dc45a9955ea6e99e62dcc6fa373864fd3533f749
Ondřej Procházka <ony.prochy@gmail.com>
2021-09-21 21:45:27 +0200

    Added comments to Program.cs

--- DailyTaskReminder/Program.cs
+++ DailyTaskReminder/Program.cs
@@ -11,10 +11,20 @@ namespace DailyTaskReminder
 {
     class Program 
     {
+        /// <summary>
+        /// List of all tasks
+        /// </summary>
         static List<Task> tasks;
 
+        /// <summary>
+        /// Http client for sending reminders using Http protocol
+        /// </summary>
         static HttpClient client = new();
 
+        /// <summary>
+        /// Start the program.
+        /// </summary>
+        /// <param name="options">Options altering the program behavior</param>
         public static void Start(Options options) 
         {
             Instances.LoadReminders(options.RemindersPath);
@@ -28,6 +38,10 @@ namespace DailyTaskReminder
             EnqueueSoonestDeadline();
         }
 
+        /// <summary>
+        /// Finds the task that should have the reminder sent soonest
+        /// and creates a timer for sending the reminder (if the task has not been finished in the meantime).
+        /// </summary>
         static void EnqueueSoonestReminder()
         {
             Task soonest = tasks
@@ -37,6 +51,10 @@ namespace DailyTaskReminder
             StartTimer(soonest.GetRemindTime, soonest, HandleReminder);
         }
 
+        /// <summary>
+        /// Finds the task with the soonest deadline and creates a timer that
+        /// resets its finish status.
+        /// </summary>
         static void EnqueueSoonestDeadline()
         {
             Task soonest = tasks
@@ -45,6 +63,12 @@ namespace DailyTaskReminder
             StartTimer(soonest.GetDeadlineTime, soonest, HandleDeadline);
         }
 
+        /// <summary>
+        /// Start the timer that will fire an action at a specific time.
+        /// </summary>
+        /// <param name="time">The time when the action should be fired</param>
+        /// <param name="task">Parameter for the action</param>
+        /// <param name="handler">Action</param>
         static void StartTimer(DateTimeOffset time, Task task, Action<Task> handler)
         {
             if (task.GetRemindTime < DateTimeOffset.Now) handler(task);
@@ -57,14 +81,17 @@ namespace DailyTaskReminder
             }
         }
 
+        /// <summary>
+        /// Possibly sends reminders and starts another timer for the next soonest reminder.
+        /// </summary>
+        /// <param name="t">Task that should send reminders if it has not been finished yet</param>
         static void HandleReminder(Task t)
         {
             if (!t.IsFinished && !t.ReminderSent)
             {
                 string message = $"Task {t.Name} should be finished in {t.GetDeadlineTime}";
-                Console.WriteLine(message);
                 t.ReminderSent = true;
-                Console.WriteLine($"{DateTimeOffset.Now} Sending reminder for {t.Name}...");
+                Console.WriteLine($"{DateTimeOffset.Now} Sending reminder(s) for {t.Name}...");
                 foreach (string reminderName in t.Reminders)
                 {
                     Instances.GetReminderByName[reminderName].Send(client, message);
@@ -74,6 +101,11 @@ namespace DailyTaskReminder
             EnqueueSoonestReminder();
         }
 
+        /// <summary>
+        /// Resets the task status after its deadline has been reached and finds the next
+        /// task with the soonest deadline.
+        /// </summary>
+        /// <param name="t"></param>
         static void HandleDeadline(Task t)
         {
             t.IsFinished = false;
